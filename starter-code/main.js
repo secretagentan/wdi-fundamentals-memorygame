20b3259804cd5eae14380984e808075fa6e2d1a4
An Nguyen <hello.an.ng@gmail.com>
2016-11-26 05:16:21 -0800

    added alerts and game logic code

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,3 +8,39 @@ var cardFour = "king";
 // console.log(cardOne == cardTwo);
 // console.log(cardTwo == cardThree);
 
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+}
+
+if (cardTwo === cardThree) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+}
+
+if (cardTwo === cardOne) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+}
+
+if (cardOne === cardFour) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+}
+
+if (cardOne === cardThree) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+}
+
+if (cardThree === cardFour) {
+	alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+}
+
